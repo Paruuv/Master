11392a730cddc0e92c3f53619ea2411f18ab2408
Paruuv <128732181+Paruuv@users.noreply.github.com>
2025-09-12 16:59:52 +0200

    12/09-update 17:00

---
+++ Jakob_SHRED/Matser_utilz.py
@@ -0,0 +1,134 @@
+from modulefinder import test
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+from pyshred import DataManager, SHRED, SHREDEngine, DeviceConfig
+import sys
+import os
+import pyshred
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+
+def trajectory_gen(data_list,lags,sensors):
+    t_dim = data_list[0].shape[0]
+    x_dim = data_list[0].shape[1]
+    trajectories = np.zeros((t_dim-lags,len(sensors),lags))
+    full_states = np.zeros((t_dim-lags,x_dim))
+    
+    for n, data_temp in enumerate(data_list):       
+        for i in range(t_dim-lags):
+            for j in range(len(sensors)):
+                trajectories[i,j,:] = data_temp[i:i+lags,sensors[j]]
+            full_states[i,:] = data_temp[i+lags,:]
+
+        if n < 1:
+            X = trajectories.copy()
+            y = full_states.copy()
+        
+        else:
+            X = np.vstack((X,trajectories))
+            y = np.vstack((y,full_states))
+    return X, y
+
+class SHREDdata(torch.utils.data.Dataset):
+    """
+    PyTorch Dataset for time series sensor data and corresponding full-state measurements.
+
+    Parameters
+    ----------
+    X : torch.Tensor
+        Input sensor sequences of shape (batch_size, lags, num_sensors).
+    Y : torch.Tensor
+        Target full-state measurements of shape (batch_size, state_dim).
+
+    Attributes
+    ----------
+    X : torch.Tensor
+        Sensor measurement sequences.
+    Y : torch.Tensor
+        Full-state target measurements.
+    len : int
+        Number of samples in the dataset.
+    """
+
+    def __init__(self, DATA):
+        """
+        Initialize the TimeSeriesDataset.
+
+        Parameters
+        ----------
+        X : torch.Tensor
+            Input sensor sequences of shape (batch_size, lags, num_sensors).
+        Y : torch.Tensor
+            Target full-state measurements of shape (batch_size, state_dim).
+        """
+        self.X = DATA['X']
+        self.Y = DATA['y']
+        self.len = self.X.shape[0]
+        
+        
+
+    def __getitem__(self, index):
+        """
+        Get a single sample from the dataset.
+
+        Parameters
+        ----------
+        index : int
+            Index of the sample to retrieve.
+
+        Returns
+        -------
+        tuple
+            (sensor_sequence, target_state) pair.
+        """
+        return self.X[index], self.Y[index]
+
+    def __len__(self):
+        """
+        Get the number of samples in the dataset.
+
+        Returns
+        -------
+        int
+            Number of samples.
+        """
+        return self.len
+     
+    def data_scaler(self,train_data):
+        print('test')
+        sc = MinMaxScaler()
+        sc.fit(train_data.X)
+        return sc
+
+    def scale_data(self,scaler,data):
+        data.X = scaler.transform(data.X)
+        return data 
+
+    def split_data(self, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1, seed=42):
+        generator = torch.Generator().manual_seed(seed)
+        train, val, test = random_split(self, [train_ratio, val_ratio, test_ratio], generator=generator)
+
+        train_data = {
+            'X':self.X[train.indices],
+            'y':self.Y[train.indices]
+        }
+        val_data = {
+            'X':self.X[val.indices],
+            'y':self.Y[val.indices]
+        }
+        test_data = {
+            'X':self.X[test.indices],
+            'y':self.Y[test.indices]
+        }
+        train_dataset = SHREDdata(train_data)
+        val_dataset = SHREDdata(val_data)
+        test_dataset = SHREDdata(test_data)
+
+        return train_dataset,val_dataset,test_dataset
+    
+    
+    
